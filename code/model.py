0ff30fa76ff7bf4626d51617e647f15c10085d72
inf2021090 <inf2021090@ionio.gr>
2024-05-09 12:15:27 +0300

    model init

--- code/model.py
+++ code/model.py
@@ -1,9 +1,34 @@
 import torch
+import torch.nn as nn
 
 
-class Unet(torch.nn.Module):
+class Conv_Block(nn.Module):
     def __init__(self):
-        super(Unet, self).__init__()
+        super(Conv_Block, self).__init__()
+
+
+    def forward(self, x):
+        pass
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+
+    def forward(self, x):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+
+    def forward(self, x):
+        pass
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
 
 
     def forward(self, x):
