44d59f3bab584f380f422012b68acafb408929fe
inf2021090 <inf2021090@ionio.gr>
2024-05-14 14:07:52 +0300

    corrected typos ,formated code

--- code/model.py
+++ code/model.py
@@ -1,19 +1,31 @@
 import torch
 import torch.nn as nn
 
-
 class Conv_Block(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, out_channels):
         super(Conv_Block, self).__init__()
+        # First convolution layer
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU(inplace=True)
 
+        # Second convolution layer
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        pass
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        return x
 
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
-
 
     def forward(self, x):
         pass
@@ -22,14 +34,77 @@ class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
 
-
     def forward(self, x):
         pass
 
 class UNet(nn.Module):
     def __init__(self):
         super(UNet, self).__init__()
+        # Down part of U-Net
+        self.down1 = Conv_Block(3, 64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.down2 = Conv_Block(64, 128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.down3 = Conv_Block(128, 256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.down4 = Conv_Block(256, 512)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
+        # Bottleneck
+        self.bottleneck = Conv_Block(512, 1024)
+
+        # Up part of U-Net
+        self.up1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.conv_up1 = Conv_Block(1024, 512)
+        self.up2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.conv_up2 = Conv_Block(512, 256)
+        self.up3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.conv_up3 = Conv_Block(256, 128)
+        self.up4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.conv_up4 = Conv_Block(128, 64)
+
+        # Final part to reduce channels to the number of classes
+        self.final = nn.Conv2d(64, 1, kernel_size=1)  # Assuming binary classification
 
     def forward(self, x):
-        pass
+        # Downsample
+        d1 = self.down1(x)
+        p1 = self.pool1(d1)
+        d2 = self.down2(p1)
+        p2 = self.pool2(d2)
+        d3 = self.down3(p2)
+        p3 = self.pool3(d3)
+        d4 = self.down4(p3)
+        p4 = self.pool4(d4)
+
+        # Bottleneck
+        bn = self.bottleneck(p4)
+
+        # Upsample and concatenate
+        u1 = self.up1(bn)
+        u1 = torch.cat([u1, d4], dim=1)
+        u1 = self.conv_up1(u1)
+
+        u2 = self.up2(u1)
+        u2 = torch.cat([u2, d3], dim=1)
+        u2 = self.conv_up2(u2)
+
+        u3 = self.up3(u2)
+        u3 = torch.cat([u3, d2], dim=1)
+        u3 = self.conv_up3(u3)
+
+        u4 = self.up4(u3)
+        u4 = torch.cat([u4, d1], dim=1)
+        u4 = self.conv_up4(u4)
+
+        # Final convolution
+        out = self.final(u4)
+        return out
+
+# Test
+if __name__ == "__main__":
+    model = UNet()
+    x = torch.randn(1, 3, 256, 256)  # ie batch size 1 n 3 color ch
+    output = model(x)
+    print(output.shape)  # result [1, 1, 256, 256] (for binary segmentation)
+
