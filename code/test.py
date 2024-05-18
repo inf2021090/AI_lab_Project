02cf4c9844245d9aab81f053a5aab1a1a54c4e1e
inf2021090 <inf2021090@ionio.gr>
2024-05-18 14:00:00 +0300

    prev test

--- code/test.py
+++ code/test.py
@@ -1,99 +1,42 @@
 import os
+import glob
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-from torchsummary import summary
 import numpy as np
-import csv
-from dataset import SatelliteImageSegmentation 
-from model import UNet 
-from utils import create_dir
+from sklearn.model_selection import train_test_split
+from dataset import SatelliteImageSegmentation
+from utils import to_categorical
+import random
+import matplotlib.pyplot as plt
 
-# Dataset
-dataset_path = '../data/dubai_dataset'
+if __name__ == "__main__":
+    path = "../data/dubai_dataset"
+    segmentation = SatelliteImageSegmentation(path)
+    image_dataset, mask_dataset, labels = segmentation.load_dataset()
+    print("Image dataset shape:", image_dataset.shape)
+    print("Mask dataset shape:", mask_dataset.shape)
+    print("Labels shape:", labels.shape)
 
-# Hyperparameters
-batch_size = 8
-num_epochs = 2
-learning_rate = 1e-4
-patch_size = 256
+    n_classes = len(np.unique(labels))
 
-model_checkpoint_path = "../saved_models"
-metrics_files_path = "../logs/metrics"
+    labels_categorical_dataset = to_categorical(labels, n_classes)
+    print(type(labels_categorical_dataset))
 
-# Mk directory if not exist
-create_dir(model_checkpoint_path)
-create_dir(metrics_files_path)
+    master_trianing_dataset = image_dataset
 
-# Instantiate the dataset
-dataset = SatelliteImageSegmentation(dataset_path, patch_size)
-image_dataset, mask_dataset, labels = dataset.load_dataset()
+    X_train, X_test, y_train, y_test = train_test_split(master_trianing_dataset, labels_categorical_dataset, test_size=0.15, random_state=100)
 
-# Create DataLoader
-train_dataset = TensorDataset(torch.tensor(image_dataset, dtype=torch.float32).permute(0, 3, 1, 2),
-                              torch.tensor(labels, dtype=torch.long))
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    print(X_train.shape)
+    print(X_test.shape)
+    print(y_train.shape)
+    print(y_test.shape)
 
-total_classes = len(np.unique(labels)) # 6 classes for segmentation
+    random_image_id = random.randint(0, len(image_dataset))
+    plt.figure(figsize=(14,8))
+    plt.subplot(121)
+    plt.imshow(image_dataset[random_image_id])
+    plt.subplot(122)
+    #plt.imshow(mask_dataset[random_image_id])
+    plt.imshow(labels[random_image_id][:,:,0])
+    plt.show()
 
-# Init model, loss function, optimizer
-model = UNet(total_classes)  
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-# Set up device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
-# CSV file path for logging loss
-csv_file_path = os.path.join(metrics_files_path, 'training_loss.csv')
-
-# Check if file exists to write the header only once
-file_exists = os.path.isfile(csv_file_path)
-
-# Training loop
-for epoch in range(num_epochs):
-    model.train()
-    running_loss = 0.0
-    for i, (images, labels) in enumerate(train_loader):
-        images, labels = images.to(device), labels.to(device)
-        
-        optimizer.zero_grad()
-        outputs = model(images)
-        
-        loss = criterion(outputs, labels.squeeze(1))
-        loss.backward()
-        optimizer.step()
-        
-        running_loss += loss.item()
-        
-        if i % 10 == 9:  # Print every 10 mini-batches
-            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
-    
-    # Log average loss per epoch
-    avg_loss = running_loss / len(train_loader)
-    print(f'Epoch [{epoch + 1}/{num_epochs}], Average Loss: {avg_loss:.4f}')
-    
-    # Save the model checkpoint after each epoch
-    model_save_path = os.path.join(model_checkpoint_path, f'unet_epoch_{epoch + 1}.pth')
-    torch.save(model.state_dict(), model_save_path)
-    
-    # Save the loss in CSV file
-    with open(csv_file_path, mode='a', newline='') as csv_file:
-        fieldnames = ['epoch', 'average_loss']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        
-        # Write the header only if the file is new
-        if not file_exists:
-            writer.writeheader()
-            file_exists = True
-        
-        # Write the metrics
-        writer.writerow({'epoch': epoch + 1, 'average_loss': avg_loss})
-
-# Save the final trained model
-final_model_save_path = os.path.join(model_checkpoint_path, 'unet_final.pth')
-torch.save(model.state_dict(), final_model_save_path)
-
-print('Training complete')
